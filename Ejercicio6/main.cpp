cbe8d33700aa6f42bc93896535cf31f173882644
Encina Fernández Rodríguez <mrencnfr@myuax.com>
2024-12-05 18:22:19 +0100

    first committ

---
+++ Ejercicio6/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Maria Encina on 05/12/2024.
+//
+#include <iostream>
+#include <sstream>
+
+int main() {
+    std::string text;
+    std::cout << "Ingresa un texto: ";
+    std::getline(std::cin, text);
+
+    std::stringstream ss;
+    ss << text;
+    std::string text_from_ss;
+    ss >> text_from_ss;
+    std::cout << "El texto desde el flujo de cadena es: " << text_from_ss << std::endl;
+    return 0;
+}
